87033d7df6e5e798fa8ce87fd01c4176c94f6ccc
happytappy <tgeorgedavid5@gmail.com>
2026-09-11 00:12:34 +0800

    Reapply "Merge branch 'main' of https://github.com/Yanndri/intramurals-game-gdg"
    This reverts commit 57a0ef25e7fee7557490a7fedacbb315783298f1.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,40 @@
+class_name TestPlayer
 extends CharacterBody2D
 
-@export var speed := 240.0
-@export var jump_velocity := -420.0
-@export var gravity := 1200.0
+## TileMap uses layers 1 and 2; StaticPlatforms uses layer 3 (bit 4).
+const TILEMAP_MASK := 0b011
+const STATIC_PLATFORM_MASK := 0b100
+const ALL_WORLD_MASK := TILEMAP_MASK | STATIC_PLATFORM_MASK
 
-func _physics_process(_delta: float) -> void:
+@export var move_speed := 180.0
+@export var jump_velocity := -340.0
+@export var drop_through_duration := 0.25
+
+@onready var platform_detector: RayCast2D = $PlatformDetector
+
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var drop_through_time_left := 0.0
+
+
+func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity.y += gravity * _delta
+		velocity.y += gravity * delta
+
+	var direction := Input.get_axis("player1left", "player1right")
+	velocity.x = move_toward(velocity.x, direction * move_speed, move_speed * 8.0 * delta)
 
 	if Input.is_action_just_pressed("player1forward") and is_on_floor():
 		velocity.y = jump_velocity
 
-	var direction := Input.get_axis("player1left", "player1right")
-	velocity.x = direction * speed
+	# Only the layer-3 ray can initiate a drop. TileMap collision remains active.
+	if Input.is_action_just_pressed("player1backward") and platform_detector.is_colliding():
+		drop_through_time_left = drop_through_duration
+		collision_mask = TILEMAP_MASK
+		velocity.y = maxf(velocity.y, 30.0)
 
-	if Input.is_action_pressed("player1backward") and not is_on_floor():
-		velocity.y += gravity * _delta
+	if drop_through_time_left > 0.0:
+		drop_through_time_left -= delta
+		if drop_through_time_left <= 0.0:
+			collision_mask = ALL_WORLD_MASK
 
 	move_and_slide()
